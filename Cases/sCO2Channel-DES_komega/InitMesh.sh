e9e6fe7a87a7a7fd6979bc91b498c052a70082db
ARattner Lab <you@example.com>
2017-06-21 13:08:19 -0400

    Updated superThermo for version 1612

--- Cases/sCO2Channel-DES_komega/InitMesh.sh
+++ Cases/sCO2Channel-DES_komega/InitMesh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#source /storage/home/mmn5264/OpenFOAM/SetupOpenFOAM-Personal.sh
+#source /storage/home/mmn5264/OpenFOAM/SetupOpenFOAM4-Personal.sh
 m4 constant/polyMesh/blockMeshDict.m4 > constant/polyMesh/blockMeshDict
 blockMesh
 mirrorMesh -noFunctionObjects -overwrite
